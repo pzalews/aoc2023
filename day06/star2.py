4bc6d593bba041c602c3b838bfe0658d856d63dc
Piotr ZALEWSKI <piotr.ZALEWSKI@idemia.com>
2023-12-06 06:31:03 +0100

    day06

---
+++ day06/star2.py
@@ -0,0 +1,71 @@
+
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    times = []
+    distances = []
+    lines = s.splitlines()
+    for line in lines:
+        if line.startswith("Time"):
+            times = [int(line.split(":")[1].replace(" ", ""))]
+        if line.startswith("Distance"):
+            distances = [int(line.split(":")[1].replace(" ", ""))]
+    # TODO: implement solution here!
+    print(times)
+    print(distances)
+
+    odp = []
+    for x in range(0, len(times)):
+        odp.append(0)
+        time = times[x]
+        for t in range(0, time):
+            d = (time-t)*t
+            if d > distances[x]:
+                odp[x] = odp[x]+1
+    print(odp)
+    result = 1
+    for x in odp:
+        result = result*x
+    return result
+
+
+INPUT_S = '''\
+Time:      7  15   30
+Distance:  9  40  200
+'''
+EXPECTED = 71503
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
