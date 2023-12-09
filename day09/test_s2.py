4f68ba164d75d0b075a3a89a621d9e1d31491721
Piotr ZALEWSKI <piotr.ZALEWSKI@idemia.com>
2023-12-09 10:41:35 +0100

    day9

---
+++ day09/test_s2.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    riddles = []
+    for line in lines:
+        r = [int(x) for x in line.split()]
+        riddles.append(r)
+    odp = []
+    for riddle in riddles:
+        prev = riddle[0]
+        sign = 1
+        while not all(x == 0 for x in riddle):
+            riddle = [j - i for i, j in zip(riddle[:-1], riddle[1:])]
+            sign = sign * -1
+            prev = prev + riddle[0] * sign
+        odp.append(prev)
+    return sum(odp)
+
+
+INPUT_S = """\
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+"""
+EXPECTED = 2
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
